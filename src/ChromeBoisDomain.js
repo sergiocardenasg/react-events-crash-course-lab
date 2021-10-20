eda3b56ab995ecab4afa1f98f5c79d6cc930d0fe
sergiocardenasg <sergiocardenas@utexas.edu>
2021-10-19 18:22:23 -0600

    Revert "removed comment"
    This reverts commit 7e716383e147485e4e51462238c61976c3a2edb7.

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -11,6 +11,13 @@ export default class ChromeBoisDomain extends Component {
   cycling = () => {
     toggleCycling()
   }
+   
+  /* TODO: Add an event listener to the `<canvas>` element to capture when a key
+  /* is pressed. When a key is pressed, an event handler should invoke the
+  /* provided `resize` function with a single argument of either '+' or '-'
+  /* if the key pressed was 'a', then it should call `resize` with '+'
+  /* if the key pressed was 's', then it should call `resize` with '-' 
+   */
 
   pressed = (event) => {
     if (event.key === 'a') {
